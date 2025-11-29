2b89d535907bb87a33d7c688934c96103d862f44
rikarika66 <pino.lee6@gmail.com>
2025-11-29 13:02:22 +0900

    Daily update

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b78461bef04cc2ab4edef932d435d535
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "db34433f8df25aba384e1eaadc99825c",
+"flutter_bootstrap.js": "42688a6c354fc8452bdc1626b0663b28",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
