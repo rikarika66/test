42f9226578af6432776648fbc319bf973fdc7784
rikarika66 <pino.lee6@gmail.com>
2026-01-14 14:21:32 +0900

    Daily update

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2343224792"
+    serviceWorkerVersion: "3384023942"
   }
 });
