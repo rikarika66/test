63c68ce8964342598d3fc56aa7fa807bc8b4e3f7
rikarika66 <pino.lee6@gmail.com>
2026-01-15 14:07:30 +0900

    Daily update

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,12 @@
 flutter build web --release --base-href "/test/"
 
 Remove-Item docs\* -Recurse -Force
- Copy-Item build\web\* docs\ -Recurse -Force
- 
- git add .
- git commit -m "Daily update"
- git push
+Copy-Item build\web\* docs\ -Recurse -Force
+
+# ★ QR公開画像を毎回復元
+New-Item -ItemType Directory -Path docs\qr_test -Force
+Copy-Item .\qr_public\* .\docs\qr_test\ -Recurse -Force
+
+git add .
+git commit -m "Daily update"
+git push
